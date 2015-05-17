1bcf79fa6cd533bea558f6b47cceaa58ecf0d9f7
Zagidulin Artyom <p0chta@yandex.ru>
2015-05-17 18:50:41 +0300

    premium factor

--- src/wotcrew.py
+++ src/wotcrew.py
@@ -6,7 +6,7 @@ from gui.Scaleform.daapi.view.meta.CrewMeta import CrewMeta
 from gui.shared import g_itemsCache
 from debug_utils import *
 
-def numWithPrefix(value):
+def numWithPostfix(value):
     if value < 1000:
         return str(int(value))
     elif value < 1000000:
@@ -21,10 +21,14 @@ def new_cm_as_tankmenResponseS(self, data):
         tankman = g_itemsCache.items.getTankman(tankmenData['tankmanID'])
         tankmanDossier = g_itemsCache.items.getTankmanDossier(tankman.invID)
         avgXp = float(tankmanDossier.getAvgXP())
-        nextLevelBattleCount = numWithPrefix(math.ceil(tankman.getNextLevelXpCost()/avgXp)) if avgXp > 0 else 'X'
-        nextSkillBattleCount = numWithPrefix(math.ceil(tankman.getNextSkillXpCost()/avgXp)) if avgXp > 0 else 'X'
-        rankPrefix = '[{0}|{1}] '.format(nextLevelBattleCount, numWithPrefix(tankman.getNextLevelXpCost()))
-        rolePrefix = '[{0}|{1}] '.format(nextSkillBattleCount, numWithPrefix(tankman.getNextSkillXpCost()))
+        if tankman.isInTank:
+            vehicle = g_itemsCache.items.getVehicle(tankman.vehicleInvID)
+            if vehicle is not None and vehicle.isPremium:
+                avgXp *= 1.5
+        nextLevelBattleCount = numWithPostfix(math.ceil(tankman.getNextLevelXpCost()/avgXp)) if avgXp > 0 else 'X'
+        nextSkillBattleCount = numWithPostfix(math.ceil(tankman.getNextSkillXpCost()/avgXp)) if avgXp > 0 else 'X'
+        rankPrefix = '[{0}|{1}] '.format(nextLevelBattleCount, numWithPostfix(tankman.getNextLevelXpCost()))
+        rolePrefix = '[{0}|{1}] '.format(nextSkillBattleCount, numWithPostfix(tankman.getNextSkillXpCost()))
         tankmenData['rank'] = rankPrefix + tankmenData['rank']
         tankmenData['role'] = rolePrefix + tankmenData['role']
     old_cm_as_tankmenResponseS(self, data)
