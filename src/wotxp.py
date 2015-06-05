65fb9fc369533abfaec0b459eeaccfe068fef567
unknown <p0chta@yandex.ru>
2015-06-05 08:55:23 +0300

    tankman macroses from config

--- src/wotxp.py
+++ src/wotxp.py
@@ -28,16 +28,16 @@ class WotXp(object):
                     with open(conf_file) as data_file:
                         self.config = json.load(data_file)
                         break
-
-def numWithPostfix(value):
-    if value < 1000:
-        return str(int(value))
-    elif value < 100000:
-        return '{:0.1f}k'.format(value/1000.0)
-    elif value < 1000000:
-        return '{0:d}k'.format(int(math.ceil(value/1000.0)))
-    else:
-        return '{:0.1f}M'.format(value/1000000.0)
+    @staticmethod
+    def numWithPostfix(value):
+        if value < 1000:
+            return str(int(value))
+        elif value < 100000:
+            return '{:0.1f}k'.format(value/1000.0)
+        elif value < 1000000:
+            return '{0:d}k'.format(int(math.ceil(value/1000.0)))
+        else:
+            return '{:0.1f}M'.format(value/1000000.0)
 
 old_cm_as_tankmenResponseS = CrewMeta.as_tankmenResponseS
 
@@ -45,21 +45,32 @@ def new_cm_as_tankmenResponseS(self, data):
     for tankmenData in data['tankmen']:
         tankman = g_itemsCache.items.getTankman(tankmenData['tankmanID'])
         tankmanDossier = g_itemsCache.items.getTankmanDossier(tankman.invID)
+        avgXp = float(tankmanDossier.getAvgXP())
+        if tankman.isInTank:
+            vehicle = g_itemsCache.items.getVehicle(tankman.vehicleInvID)
+            if vehicle is not None and vehicle.isPremium:
+                avgXp = (avgXp + int(avgXp*(9 - max(min(vehicle.level, 9), 2))/10.0))*1.5
+        nextLevelBattleCount = wotxp.numWithPostfix(math.ceil(tankman.getNextLevelXpCost()/avgXp)) if avgXp > 0 else 'X'
+        nextSkillBattleCount = wotxp.numWithPostfix(math.ceil(tankman.getNextSkillXpCost()/avgXp)) if avgXp > 0 else 'X'
+        values = {}
+        values['freeXp'] = wotxp.numWithPostfix(tankman.descriptor.freeXP)
+        values['nextLevelBattleCount'] = nextLevelBattleCount
+        values['nextSkillBattleCount'] = nextSkillBattleCount
+        values['nextLevelXpCost'] = wotxp.numWithPostfix(tankman.getNextLevelXpCost())
+        values['nextSkillXpCost'] = wotxp.numWithPostfix(tankman.getNextSkillXpCost())
+        rankPrefix = ''
+        rolePrefix = ''
         if tankman.hasNewSkill:
-            rankPrefix = '[+{0}] '.format(numWithPostfix(tankman.descriptor.freeXP))
-            tankmenData['rank'] = rankPrefix + tankmenData['rank']
+            rankPrefix = wotxp.config.get("tankmanNewSkillRankPrefix", "[+{{freeXp}}]")
+            rolePrefix = wotxp.config.get("tankmanNewSkillRolePrefix", "")
         else:
-            avgXp = float(tankmanDossier.getAvgXP())
-            if tankman.isInTank:
-                vehicle = g_itemsCache.items.getVehicle(tankman.vehicleInvID)
-                if vehicle is not None and vehicle.isPremium:
-                    avgXp = (avgXp + int(avgXp*(9 - max(min(vehicle.level, 9), 2))/10.0))*1.5
-            nextLevelBattleCount = numWithPostfix(math.ceil(tankman.getNextLevelXpCost()/avgXp)) if avgXp > 0 else 'X'
-            nextSkillBattleCount = numWithPostfix(math.ceil(tankman.getNextSkillXpCost()/avgXp)) if avgXp > 0 else 'X'
-            rankPrefix = '[{0}|{1}] '.format(nextLevelBattleCount, numWithPostfix(tankman.getNextLevelXpCost()))
-            rolePrefix = '[{0}|{1}] '.format(nextSkillBattleCount, numWithPostfix(tankman.getNextSkillXpCost()))
-            tankmenData['rank'] = rankPrefix + tankmenData['rank']
-            tankmenData['role'] = rolePrefix + tankmenData['role']
+            rankPrefix = wotxp.config.get("tankmanRankPrefix", "[{{nextLevelBattleCount}}|{{nextLevelXpCost}}]")
+            rolePrefix = wotxp.config.get("tankmanRolePrefix", "[{{nextSkillBattleCount}}|{{nextSkillXpCost}}]")
+        for key in values.keys():
+            rankPrefix = rankPrefix.replace('{{%s}}' % key, values[key])
+            rolePrefix = rolePrefix.replace('{{%s}}' % key, values[key])
+        tankmenData['rank'] = rankPrefix + tankmenData['rank']
+        tankmenData['role'] = rolePrefix + tankmenData['role']
     old_cm_as_tankmenResponseS(self, data)
 
 CrewMeta.as_tankmenResponseS = new_cm_as_tankmenResponseS
@@ -70,6 +81,8 @@ def new_rpm_as_updateCurrentVehicleS(self, name, type, vDescription, earnedXP, i
     if isElite or not g_currentVehicle.isPresent():
         old_rpm_as_updateCurrentVehicleS(self, name, type, vDescription, earnedXP, isElite, isPremIGR)
         return
+    if wotxp.config.get('debug', False):
+        LOG_NOTE(g_currentVehicle.item.n)
     vehDoss = g_itemsCache.items.getVehicleDossier(g_currentVehicle.item.intCD)
     avgXp = vehDoss.getRandomStats().getAvgXP()
     if not avgXp:
@@ -101,11 +114,11 @@ def new_rpm_as_updateCurrentVehicleS(self, name, type, vDescription, earnedXP, i
         requiredXp = max(requiredEliteXp - xp, 0)
         if wotxp.config.get("useFreeXpForVehicleResearch", False):
             requiredXp = max(requiredXp - freeXP, 0)
-    battleCount = numWithPostfix(math.ceil(requiredXp/avgXp)) if avgXp > 0 else 'X'
+    battleCount = wotxp.numWithPostfix(math.ceil(requiredXp/avgXp)) if avgXp > 0 else 'X'
     description = vDescription +\
         ' [ {0} <img align="top" src="img://gui/maps//icons/library/BattleResultIcon-1.png" height="14" width="14" vspace="-3"/>'\
         ' {1} <img align="top" src="img://gui/maps//icons/library/XpIcon-1.png" height="16" width="16" vspace="-3"/>]'.\
-        format(battleCount, numWithPostfix(requiredXp))
+        format(battleCount, wotxp.numWithPostfix(requiredXp))
     old_rpm_as_updateCurrentVehicleS(self, name, type, description, earnedXP, isElite, isPremIGR)
 
 ResearchPanelMeta.as_updateCurrentVehicleS = new_rpm_as_updateCurrentVehicleS
